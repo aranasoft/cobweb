325aed833849db799af41cf536dc9afeda03bc17
Jay Harris <jay@aranasoft.com>
2013-12-09 10:18:35 -0500

    Added extension method for getting Description from Enum attribute

---
+++ src/Core/Extentions/EnumExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Aranasoft.Cobweb.Extentions {
+    public static class EnumExtensions {
+        public static string GetDescription(this Enum enumeration)
+        {
+            var type = enumeration.GetType();
+            var enumMemberInfo = type.GetMember(enumeration.ToString());
+
+            if (!enumMemberInfo.Any()) return enumeration.ToString();
+
+            var attributes = enumMemberInfo.First().GetCustomAttributes(typeof (DescriptionAttribute), false);
+            if (attributes.Any())
+                return ((DescriptionAttribute) attributes.First()).Description;
+
+            return enumeration.ToString();
+        }
+    }
+}
