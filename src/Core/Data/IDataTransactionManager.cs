42ec684e03f6403730a51b8d4c206ffd5007a39a
Jay Harris <jay@aranasoft.com>
2014-01-20 13:37:14 -0500

    Transaction Manager to execute a unit of work

--- src/Core/Data/IDataTransactionManager.cs
+++ src/Core/Data/IDataTransactionManager.cs
@@ -1,9 +1,49 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 using Cobweb.DependencyInjection;
 
 namespace Cobweb.Data {
     public interface IDataTransactionManager : IDependency {
         IDataTransaction BeginTransaction();
         IDataTransaction BeginTransaction(IsolationLevel isolationLevel);
+    }
+
+    public abstract class DataTransactionManager : IDataTransactionManager {
+        public abstract IDataTransaction BeginTransaction();
+        public abstract IDataTransaction BeginTransaction(IsolationLevel isolationLevel);
+
+         public virtual TEntity ExecuteTransaction<TEntity, TIdentifier>(TEntity entity, Action<TEntity> work)
+            where TEntity : Entity<TEntity, TIdentifier>, IEquatable<TEntity>, new()
+            where TIdentifier : struct, IComparable, IComparable<TIdentifier>, IEquatable<TIdentifier>
+        {
+            using (IDataTransaction tx = BeginTransaction())
+            {
+                try
+                {
+                    work.Invoke(entity);
+                    tx.Commit();
+                    return entity;
+                }
+                catch (Exception)
+                {
+                    tx.Rollback();
+                    throw;
+                }
+            }
+        }
+
+        public virtual void ExecuteTransaction(Action work)
+        {
+            using (IDataTransaction tx = BeginTransaction()) {
+                try {
+                    work.Invoke();
+                    tx.Commit();
+                }
+                catch (Exception) {
+                    tx.Rollback();
+                    throw;
+                }
+            }
+        }
     }
 }
--- src/Data.NHibernate/INHibernateSessionBuilder.cs
+++ src/Data.NHibernate/INHibernateSessionBuilder.cs
@@ -4,4 +4,8 @@ namespace Cobweb.Data.NHibernate {
     public interface INHibernateSessionBuilder : IDataTransactionManager {
         ISession GetCurrentSession();
     }
+
+    public abstract class NHibernateSessionBuilder : DataTransactionManager, INHibernateSessionBuilder {
+        public abstract ISession GetCurrentSession();
+    }
 }
