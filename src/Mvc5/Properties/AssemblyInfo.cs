f75b45b6442bfae97d1bcccca1fe5abf055ede31
Jay Harris <jay@aranasoft.com>
2014-01-20 13:06:58 -0500

    Configure library descriptions. Configure NuGet packages.

--- src/Mvc5/Properties/AssemblyInfo.cs
+++ src/Mvc5/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Cobweb.Web.Mvc")]
-[assembly: AssemblyDescription("Cobweb is a base-class utility library for .NET")]
+[assembly: AssemblyDescription("MVC5 utilities and classes for Cobweb, a base-class utility library for .NET")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Arana Software")]
 [assembly: AssemblyProduct("Aranasoft's Cobweb")]
