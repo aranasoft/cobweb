2c603a6d9f42c0c3d7e93b7fc7b90fb7e077eb31
Jay Harris <jay@aranasoft.com>
2014-01-22 11:14:25 -0500

    Fixed package framework references for dependencies.

---
+++ src/Tests/Data.NHibernate.Tests/CompileTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using Cobweb.Data;
+using Cobweb.Data.NHibernate;
+using NHibernate;
+
+namespace Data.NHibernate.Tests {
+    /// <summary>
+    ///     These tests are not executed by NUnit. They are here for compile-time checking. 'If it builds, ship it.'
+    /// </summary>
+    public class CompileTests {
+        internal class SessionBuilder : NHibernateSessionBuilder {
+            public override IDataTransaction BeginTransaction() {
+                return new NHibernateTransactionHandler(GetCurrentSession().BeginTransaction());
+            }
+
+            public override IDataTransaction BeginTransaction(IsolationLevel isolationLevel) {
+                return new NHibernateTransactionHandler(GetCurrentSession().BeginTransaction());
+            }
+
+            public override ISession GetCurrentSession() {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
--- src/Tests/Data.NHibernate.Tests/Properties/AssemblyInfo.cs
+++ src/Tests/Data.NHibernate.Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Cobweb.Data.NHibernate")]
-[assembly: AssemblyDescription("NHibernate utilities and classes for Cobweb, a base-class utility library for .NET")]
+[assembly: AssemblyTitle("Data.NHibernate.Tests")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Arana Software")]
-[assembly: AssemblyProduct("Aranasoft's Cobweb")]
-[assembly: AssemblyCopyright("Copyright © 2013-2014")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Data.NHibernate.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("794b5fa1-9174-4620-b19b-56cc3d12ecd7")]
+[assembly: Guid("16131f11-683a-4136-b326-cd2cf6fd4f45")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3")]
-[assembly: AssemblyFileVersion("1.0.3")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
