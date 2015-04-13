ecb7b7595f66d3cf198d37b61c4290aaaa32660f
Jay Harris <jay@aranasoft.com>
2015-04-13 10:50:31 -0400

    docs(extentions): Add XML Code Documentation for Cobweb extension methods.

--- src/Core/Extentions/WithEnum.cs
+++ src/Core/Extentions/WithEnum.cs
@@ -5,6 +5,15 @@ using System.Reflection;
 
 namespace Cobweb.Extentions {
     public static class WithEnum {
+        /// <summary>
+        ///     Returns the value of an enum element's DescriptionAttribute or the name of the element.
+        /// </summary>
+        /// <example>
+        ///     <code> System.Reflection.BindingFlags.Public.GetDescription(); </code>
+        /// </example>
+        /// <remarks>Uses the value within a System.ComponentModel.DescriptionAttribute, if available.</remarks>
+        /// <param name="enumeration">The enum element.</param>
+        /// <returns>The identified element description.</returns>
         public static string GetDescription(this Enum enumeration) {
             Type type = enumeration.GetType();
             MemberInfo[] enumMemberInfo = type.GetMember(enumeration.ToString());
--- src/Core/Extentions/WithType.cs
+++ src/Core/Extentions/WithType.cs
@@ -2,6 +2,12 @@
 
 namespace Cobweb.Extentions {
     public static class WithType {
+        /// <summary>
+        /// Identify if a class derives from a specified generic base class.
+        /// </summary>
+        /// <param name="checkType">The derived type to analyse.</param>
+        /// <param name="genericType">The generic base type to check against.</param>
+        /// <returns>True if <paramref name="checkType" /> derives from <paramref name="genericType" />, otherwise False.</returns>
         public static bool IsAssignableToGeneric(this Type checkType, Type genericType) {
             if (genericType == null) {
                 throw new ArgumentNullException("genericType");
