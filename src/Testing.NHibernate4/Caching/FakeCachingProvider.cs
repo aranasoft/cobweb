33d85636d90da5849660222a49a9e4c9dc5732e6
Jay Harris <jay@aranasoft.com>
2017-07-07 13:58:47 -0400

    feat(cache): add testing version of NHibernate Caching provider

---
+++ src/Testing.NHibernate4/Caching/FakeCachingProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using Cobweb.Data.NHibernate.Caching;
+
+namespace Cobweb.Testing.NHibernate.Caching {
+    public class FakeCachingProvider : ICachingProvider {
+        public ICacheRequest<T> Cacheable<T>(
+            IQueryable<T> query) {
+            return new CacheRequest<T>(query);
+        }
+    }
+}
