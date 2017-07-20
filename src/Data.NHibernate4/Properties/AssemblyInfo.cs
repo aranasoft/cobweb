68d58e7fa62657c96eb59efdf6d31559de23dae1
Jay Harris <jay@aranasoft.com>
2017-07-20 14:14:39 -0400

    chore(ver): Data.NHibernate v1.40.2

--- src/Data.NHibernate4/Properties/AssemblyInfo.cs
+++ src/Data.NHibernate4/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.40.2")]
-[assembly: AssemblyInformationalVersion("1.40.2-beta3")]
+[assembly: AssemblyInformationalVersion("1.40.2")]
 [assembly: AssemblyFileVersion("1.40.2")]
