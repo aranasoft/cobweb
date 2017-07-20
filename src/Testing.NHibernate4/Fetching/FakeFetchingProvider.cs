5856568b0f546091df0275a38e38cca5574a6475
Jay Harris <jay@aranasoft.com>
2017-07-20 13:47:03 -0400

    fix(fetch): support ThenFetch / ThenFetchMany on Eager Load in Testing

--- src/Testing.NHibernate4/Fetching/FakeFetchingProvider.cs
+++ src/Testing.NHibernate4/Fetching/FakeFetchingProvider.cs
@@ -6,30 +6,29 @@ using Cobweb.Data.NHibernate.Fetching;
 
 namespace Cobweb.Testing.NHibernate.Fetching {
     public class FakeFetchingProvider : IFetchingProvider {
-        public IFetchRequest<T> Fetch<T, TProperty>(
-            IQueryable<T> query,
-            Expression<Func<T, TProperty>> relatedObjectSelector) {
-            return new FetchRequest<IQueryable<T>, T>(query);
+        public IFetchRequest<TOriginatingEntity, TFetch> Fetch<TOriginatingEntity, TFetch>(
+            IQueryable<TOriginatingEntity> source, Expression<Func<TOriginatingEntity, TFetch>> path) {
+            return new FetchRequest<IQueryable<TOriginatingEntity>, TOriginatingEntity, TFetch>(source);
         }
 
-        public IFetchRequest<T> FetchMany<T, TProperty>(
-            IQueryable<T> query,
-            Expression<Func<T, IEnumerable<TProperty>>> relatedObjectSelector) {
-            return new FetchRequest<IQueryable<T>, T>(query);
+        public IFetchRequest<TOriginatingEntity, TFetch> FetchMany<TOriginatingEntity, TFetch>(
+            IQueryable<TOriginatingEntity> source, Expression<Func<TOriginatingEntity, IEnumerable<TFetch>>> path) {
+            return new FetchRequest<IQueryable<TOriginatingEntity>, TOriginatingEntity, TFetch>(source);
         }
 
-        public IFetchRequest<T> ThenFetch<T, TFetchedProperty, TChildProperty>(
-            IFetchRequest<T> query,
-            Expression<Func<TFetchedProperty, TChildProperty>> relatedObjectSelector) {
-            var fetchRequest = (FetchRequest<IQueryable<T>, T>) query;
-            return new FetchRequest<IQueryable<T>, T>(fetchRequest.Queryable);
+        public IFetchRequest<TOriginatingEntity, TNestedFetch> ThenFetch<TOriginatingEntity, TFetchOn, TNestedFetch>(
+            IFetchRequest<TOriginatingEntity, TFetchOn> source,
+            Expression<Func<TFetchOn, TNestedFetch>> relatedObjectSelector) {
+            var fetchRequest = (FetchRequest<IQueryable<TOriginatingEntity>, TOriginatingEntity, TNestedFetch>) source;
+            return new FetchRequest<IQueryable<TOriginatingEntity>, TOriginatingEntity, TNestedFetch>(fetchRequest.Queryable);
         }
 
-        public IFetchRequest<T> ThenFetchMany<T, TFetchedProperty, TChildProperty>(
-            IFetchRequest<T> query,
-            Expression<Func<TFetchedProperty, IEnumerable<TChildProperty>>> relatedObjectSelector) {
-            var fetchRequest = (FetchRequest<IQueryable<T>, T>) query;
-            return new FetchRequest<IQueryable<T>, T>(fetchRequest.Queryable);
+        public IFetchRequest<TOriginatingEntity, TNestedFetch>
+            ThenFetchMany<TOriginatingEntity, TFetchOn, TNestedFetch>(
+                IFetchRequest<TOriginatingEntity, TFetchOn> source,
+                Expression<Func<TFetchOn, IEnumerable<TNestedFetch>>> path) {
+            var fetchRequest = (FetchRequest<IQueryable<TOriginatingEntity>, TOriginatingEntity, TFetchOn>) source;
+            return new FetchRequest<IQueryable<TOriginatingEntity>, TOriginatingEntity, TNestedFetch>(fetchRequest.Queryable);
         }
     }
 }
