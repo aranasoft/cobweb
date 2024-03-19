11dff20aa9046f37d3786cd1b1fabcd7da3d3a76
Jay Harris <jay@aranasoft.com>
2024-03-19 09:24:59 -0700

    feat(efcore): support EF Core v8

---
+++ src/entityframeworkcore/test/entityframeworkcore8.validation.tests/Support/SqlServer/SqlServerLocalDbFixture.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Data;
+using System.Data.Common;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.SqlServer.Design.Internal;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aranasoft.Cobweb.EntityFrameworkCore.Validation.Tests.Support.SqlServer {
+    public class SqlServerLocalDbFixture : IDisposable {
+        public ApplicationDbContext GetContext() {
+            return new ApplicationDbContext(_builder.Options);
+        }
+
+        private readonly LocalDbTestingDatabase _testingDatabase;
+        private readonly DbContextOptionsBuilder<ApplicationDbContext> _builder;
+        private readonly DbConnection _dbConnection;
+
+        public SqlServerLocalDbFixture() {
+            var serviceCollection = new ServiceCollection().AddEntityFrameworkDesignTimeServices();
+            new SqlServerDesignTimeServices().ConfigureDesignTimeServices(serviceCollection);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            _testingDatabase = new LocalDbTestingDatabase();
+            _testingDatabase.EnsureDatabase();
+
+            var connectionString = _testingDatabase.ConnectionString;
+            _dbConnection = new SqlConnection(connectionString);
+            _dbConnection.Open();
+
+            _builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            _builder.UseSqlServer(_dbConnection);
+            _builder.UseApplicationServiceProvider(serviceProvider);
+        }
+
+        public virtual void Dispose() {
+            if (_dbConnection.State == ConnectionState.Open) _dbConnection.Close();
+            _dbConnection.Dispose();
+
+            _testingDatabase.Dispose();
+        }
+    }
+}
