3c9df946402a97706dd7ed17e3974ddebafc1ffd
Jay Harris <jay@aranasoft.com>
2024-04-02 14:55:37 -0700

    feat(azure): add support for .NET 8

--- src/azure/src/azure/ServiceBus/TaskQueue/TaskRequestException.cs
+++ src/azure/src/azure/ServiceBus/TaskQueue/TaskRequestException.cs
@@ -6,6 +6,8 @@ namespace Aranasoft.Cobweb.Azure.ServiceBus.TaskQueue {
         public TaskRequestException() { }
         public TaskRequestException(string message) : base(message) {}
         public TaskRequestException(string message, Exception innerException) : base(message,innerException) {}
+#if !NET8_0_OR_GREATER
         public TaskRequestException(SerializationInfo info, StreamingContext context) : base(info, context) {}
+#endif
     }
 }
