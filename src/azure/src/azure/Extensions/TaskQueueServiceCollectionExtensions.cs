12ed38826173fd6a3f81cb7c6570a49c71dadb61
Jay Harris <jay@aranasoft.com>
2024-04-03 10:53:39 -0700

    docs(azure): improve documentation for dependency injection methods

--- src/azure/src/azure/Extensions/TaskQueueServiceCollectionExtensions.cs
+++ src/azure/src/azure/Extensions/TaskQueueServiceCollectionExtensions.cs
@@ -14,8 +14,11 @@ namespace Microsoft.Extensions.DependencyInjection {
     /// </summary>
     public static class TaskQueueServiceCollectionExtensions {
         /// <summary>
-        ///   Registers a <see cref="TaskRequestQueue "/> instance with the provided <paramref name="connectionString"/>.
+        ///   Registers a <see cref="TaskRequestQueue "/> instance with the provided connection string.
         /// </summary>
+        /// <param name="builder">The builder to register the client factory with.</param>
+        /// <param name="connectionString">The connection string to the Azure Service Bus.</param>
+        /// <returns>An Azure client builder for chaining further configuration.</returns>
         public static IAzureClientBuilder<TaskRequestQueue, TaskRequestQueueOptions> AddTaskQueue<TBuilder>(this TBuilder builder, string connectionString)
         where TBuilder : IAzureClientFactoryBuilder
         {
@@ -23,8 +26,11 @@ namespace Microsoft.Extensions.DependencyInjection {
         }
 
         /// <summary>
-        ///   Registers a <see cref="TaskRequestQueue"/> instance with the provided <paramref name="fullyQualifiedNamespace"/>.
+        ///   Registers a <see cref="TaskRequestQueue"/> instance with the provided fully qualified namespace.
         /// </summary>
+        /// <param name="builder">The builder to register the client factory with.</param>
+        /// <param name="fullyQualifiedNamespace">The fully qualified namespace of the Azure Service Bus.</param>
+        /// <returns>An Azure client builder for chaining further configuration.</returns>
         public static IAzureClientBuilder<TaskRequestQueue, TaskRequestQueueOptions> AddTaskQueueWithNamespace<TBuilder>(this TBuilder builder, string fullyQualifiedNamespace)
         where TBuilder : IAzureClientFactoryBuilderWithCredential
         {
@@ -32,8 +38,11 @@ namespace Microsoft.Extensions.DependencyInjection {
         }
 
         /// <summary>
-        ///   Registers a <see cref="TaskRequestQueue"/> instance with connection options loaded from the provided <paramref name="configuration"/> instance.
+        ///   Registers a <see cref="TaskRequestQueue"/> instance with connection options loaded from the provided configuration instance.
         /// </summary>
+        /// <param name="builder">The builder to register the client factory with.</param>
+        /// <param name="configuration">The configuration instance containing connection options.</param>
+        /// <returns>An Azure client builder for chaining further configuration.</returns>
         public static IAzureClientBuilder<TaskRequestQueue, TaskRequestQueueOptions> AddTaskQueue<TBuilder, TConfiguration>(this TBuilder builder, TConfiguration configuration)
         where TBuilder : IAzureClientFactoryBuilderWithConfiguration<TConfiguration>
         {
@@ -41,7 +50,7 @@ namespace Microsoft.Extensions.DependencyInjection {
         }
 
         /// <summary>
-        ///     Registers the task queue processing services and handlers in the <see cref="IServiceCollection" />.
+        ///   Registers the task queue processing services and handlers in the <see cref="IServiceCollection" />.
         /// </summary>
         /// <param name="serviceCollection">The <see cref="IServiceCollection" /> to add services to.</param>
         /// <param name="assemblies">The assemblies to scan for types deriving from <see cref="TaskHandler{TTaskRequest}" />.</param>
@@ -57,16 +66,33 @@ namespace Microsoft.Extensions.DependencyInjection {
             return serviceCollection;
         }
 
+        /// <summary>
+        ///   Registers the task queue processor services in the <see cref="IServiceCollection" />.
+        /// </summary>
+        /// <param name="serviceCollection">The <see cref="IServiceCollection" /> to add services to.</param>
+        /// <returns>The same service collection so that multiple calls can be chained.</returns>
         private static IServiceCollection AddTaskQueueProcessorServices(this IServiceCollection serviceCollection) =>
             serviceCollection.AddTransient<ITaskHandlerResolver, TaskHandlerResolver>()
             .AddTransient<ITaskCoordinator, TaskCoordinator>();
 
+        /// <summary>
+        ///   Registers task handlers from the provided assemblies in the <see cref="IServiceCollection" />.
+        /// </summary>
+        /// <param name="serviceCollection">The <see cref="IServiceCollection" /> to add services to.</param>
+        /// <param name="assemblies">The assemblies to scan for task handlers.</param>
+        /// <returns>The same service collection so that multiple calls can be chained.</returns>
         private static IServiceCollection AddTaskHandlersFromAssemblies(
         this IServiceCollection serviceCollection,
         params Assembly[] assemblies) {
             return assemblies.Aggregate(serviceCollection, AddTaskHandlersFromAssembly);
         }
 
+        /// <summary>
+        ///   Registers task handlers from the provided assembly in the <see cref="IServiceCollection" />.
+        /// </summary>
+        /// <param name="serviceCollection">The <see cref="IServiceCollection" /> to add services to.</param>
+        /// <param name="assembly">The assembly to scan for task handlers.</param>
+        /// <returns>The same service collection so that multiple calls can be chained.</returns>
         private static IServiceCollection AddTaskHandlersFromAssembly(
         this IServiceCollection serviceCollection,
         Assembly assembly) {
@@ -74,13 +100,30 @@ namespace Microsoft.Extensions.DependencyInjection {
             return serviceCollection.AddTaskHandlers(handlerTypes);
         }
 
+        /// <summary>
+        ///   Registers task handlers from the provided types in the <see cref="IServiceCollection" />.
+        /// </summary>
+        /// <param name="serviceCollection">The <see cref="IServiceCollection" /> to add services to.</param>
+        /// <param name="taskHandlerTypes">The types of the task handlers to register.</param>
+        /// <returns>The same service collection so that multiple calls can be chained.</returns>
         public static IServiceCollection AddTaskHandlers(
         this IServiceCollection serviceCollection,
         IEnumerable<Type> taskHandlerTypes) => taskHandlerTypes.Aggregate(serviceCollection, AddTaskHandler);
 
+        /// <summary>
+        ///   Registers a task handler of the provided type in the <see cref="IServiceCollection" />.
+        /// </summary>
+        /// <param name="serviceCollection">The <see cref="IServiceCollection" /> to add services to.</param>
+        /// <returns>The same service collection so that multiple calls can be chained.</returns>
         public static IServiceCollection AddTaskHandler<TTaskHandler, TTaskRequest>(this IServiceCollection serviceCollection) where TTaskHandler : TaskHandler<TTaskRequest> where TTaskRequest : TaskRequest
             => serviceCollection.AddTaskHandler(typeof(TTaskHandler));
 
+        /// <summary>
+        ///   Registers a task handler of the provided type in the <see cref="IServiceCollection" />.
+        /// </summary>
+        /// <param name="serviceCollection">The <see cref="IServiceCollection" /> to add services to.</param>
+        /// <param name="taskHandlerType">The type of the task handler to register.</param>
+        /// <returns>The same service collection so that multiple calls can be chained.</returns>
         public static IServiceCollection AddTaskHandler(
         this IServiceCollection serviceCollection,
         Type taskHandlerType) {
