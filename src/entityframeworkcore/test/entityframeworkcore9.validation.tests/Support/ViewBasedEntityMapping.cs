dcea139d678b4d19f9283698ea5738f97db7f78f
Jay Harris <jay@aranasoft.com>
2025-06-17 22:51:59 -0700

    feat(efcore): support .NET 9

---
+++ src/entityframeworkcore/test/entityframeworkcore9.validation.tests/Support/ViewBasedEntityMapping.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Aranasoft.Cobweb.EntityFrameworkCore.Validation.Tests.Support {
+    public class ViewBasedEntityMapping : IEntityTypeConfiguration<ViewBasedEntity> {
+        public void Configure(EntityTypeBuilder<ViewBasedEntity> builder) {
+            builder.ToView("ViewBasedEntities").ToTable((string?) null);
+            builder.HasOne(entity => entity.TableEntity).WithMany().HasForeignKey(view => view.Id);
+            builder.HasOne(entity => entity.Role).WithMany();
+        }
+    }
+}
